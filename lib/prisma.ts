5ebdea379fe563db9ae2a46e8d687e1902a4d189
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-27 17:28:52 +0545

    initialization prisma.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,12 @@
-import { PrismaClient } from "@prisma/client/extension";
+// lib/prisma.ts
+import { PrismaClient } from '@prisma/client';
 
-const prismaClientSingleton = () => { 
-    return new PrismaClient();
-}
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  });
 
-const globalForPrisma = globalThis as unknown as { 
-    prisma: PrismaClientSingleton | undefined
-}
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton
-
-export default prisma
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
