0958a621b870a5ac3382c194fff837950ce349c1
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-29 15:03:53 +0545

    fix fix fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as typeof global & { prisma?: PrismaClient };
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
+}
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query', 'info', 'warn', 'error'], 
-  });
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['query'],
+    })
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
