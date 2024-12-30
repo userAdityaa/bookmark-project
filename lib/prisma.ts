5a4a9be619203022281452202a11635a6f82e620
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-30 13:09:51 +0545

    udpate update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,24 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient()
-  }
-  prisma = (global as any).prisma
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined
 }
 
-prisma.$connect()
-  .catch((error) => {
-    console.error('Failed to connect to the database:', error)
-    process.exit(1)
-  })
+export const prisma =
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ['query'],
+    })
 
-process.on('beforeExit', async () => {
-  await prisma.$disconnect()
-})
-
-export default prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
