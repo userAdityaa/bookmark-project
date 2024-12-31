99c3145bd02a52f2e886a774e64264a2357d371f
userAdityaa <aditya.chaudhary1558@gmail.com>
2024-12-31 16:08:00 +0545

    login_page: fixed border

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -89,7 +89,7 @@ export default function SignIn() {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-[#282828] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center border border-white"
+                className="w-full text-white bg-[#282828] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Login
               </button>
